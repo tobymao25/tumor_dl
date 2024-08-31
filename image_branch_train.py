c49339e995317c0932b9c9626b240f3241ff782f
Linda  Tang <ltang35@mrphpch1.cm.cluster>
2024-08-30 21:07:07 -0400

    changed fp

--- image_branch_train.py
+++ image_branch_train.py
@@ -169,8 +169,8 @@ def train_model(config):
             optimizer.zero_grad()
             reconstruction, latent_params = model(inputs)
             if epoch % 10 == 0 & i == 0:
-                torch.save(inputs,  f'inputs_tensor_epoch{epoch}.pt')
-                torch.save(reconstruction, f'reconstruction_tensor_epoch{epoch}.pt')
+                torch.save(inputs,  f'/home/ltang35/tumor_dl/TrainingDataset/inputs_tensor_epoch{epoch}.pt')
+                torch.save(reconstruction, f'/home/ltang35/tumor_dl/TrainingDataset/reconstruction_tensor_epoch{epoch}.pt')
             total_loss, rec_loss, surv_loss, MSE = compute_combined_loss(
                 reconstruction, inputs, latent_params, survival_times,
                 reconstruction_loss, survival_loss, delta
