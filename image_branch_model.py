826a76dbe86420b03949341babab3d3c44781e37
tobymao25 <tobymao25@gmail.com>
2024-09-13 14:36:06 +0200

    new unet

--- image_branch_model.py
+++ image_branch_model.py
@@ -4,6 +4,18 @@ import numpy as np
 import torch.nn.functional as F
 from image_branch_utils import GaussianNoise
 
+def downsample_fn(depth):
+    if depth == 6:
+        return [(2, 2, 2)] * 6 
+    elif depth == 8:
+        return [(1.5, 1.5, 1.5)] * 8 
+    elif depth == 4:
+        return [(2, 2, 2), (2, 2, 2), (2, 2, 2), (2, 2, 2)]  # Increase depth upsample factor to 2
+    elif depth == 2:
+        return [(4, 4, 4), (4, 4, 4)]  # Increase depth upsample factor to 4 if needed
+    else:
+        raise ValueError(f'Unsupported depth: {depth}')
+
 
 class encoder(nn.Module):
     def __init__(self, input_shape, network_depth, no_convolutions, conv_filter_no_init, 
@@ -23,7 +35,8 @@ class encoder(nn.Module):
         self.activation = activation  
         self.encoder_layers = nn.ModuleList()
         self.gaussian_noise_factor = gaussian_noise_factor
-        
+        self.skip_connections = []
+
         # Gaussian noise layer
         if gaussian_noise_factor:
             self.noise_layer = GaussianNoise()
@@ -33,18 +46,23 @@ class encoder(nn.Module):
         # Convolutional layers
         in_channels = self.input_shape[0]
         for i in range(network_depth):
+            conv_layers = []
             for j in range(no_convolutions):
-                out_channels = self.conv_filter_no_init * (2 ** i)
+                out_channels = self.conv_filter_no_init * (2 ** i) 
                 conv_layer = nn.Conv3d(in_channels, out_channels, self.conv_kernel_size, padding=(conv_kernel_size-1) // 2)
-                self.encoder_layers.append(conv_layer)
+                conv_layers.append(conv_layer)
                 if self.use_batch_normalization:
-                    self.encoder_layers.append(nn.BatchNorm3d(out_channels))
+                    conv_layers.append(nn.BatchNorm3d(out_channels))
                 if self.activation == 'leakyrelu':
-                    self.encoder_layers.append(nn.LeakyReLU(inplace=True))
+                    conv_layers.append(nn.LeakyReLU(inplace=True))
                 else:
-                    self.encoder_layers.append(nn.ReLU(inplace=True))
+                    conv_layers.append(nn.ReLU(inplace=True))
                 in_channels = out_channels
+            
+            self.encoder_layers.extend(conv_layers)
             self.encoder_layers.append(nn.MaxPool3d(kernel_size=2, stride=2))
+            self.skip_connections.append(conv_layers[-1])  
+
             if dropout_value:
                 self.encoder_layers.append(nn.Dropout3d(p=dropout_value))
 
@@ -67,37 +85,32 @@ class encoder(nn.Module):
     def _forward_conv_layers(self, x):
         if self.noise_layer:
             x = self.noise_layer(x)
+        
+        skip_connections = []  
+
         for layer in self.encoder_layers:
             x = layer(x)
+            if isinstance(layer, nn.MaxPool3d): 
+                skip_connections.append(x.clone())
+
+        self.skip_connections = skip_connections  
         return x
     
     def forward(self, x):
         x = self._forward_conv_layers(x)
         x = self.flatten(x)
         x = self.fc(x)
+        print(f'this is the shape of fully connected layer: {x.shape}')
         x = self.activation_fn(x)
         return x
 
 
-def downsample_fn(depth):
-    if depth == 6:
-        return [(2, 2, 2)] * 6 
-    elif depth == 8:
-        return [(1.5, 1.5, 1.5)] * 8 
-    elif depth == 4:
-        return [(2, 2, 2), (2, 2, 2), (2, 2, 2), (2, 2, 2)]  # Increase depth upsample factor to 2
-    elif depth == 2:
-        return [(4, 4, 4), (4, 4, 4)]  # Increase depth upsample factor to 4 if needed
-    else:
-        raise ValueError(f'Unsupported depth: {depth}')
-
-
 class Decoder3D(nn.Module):
     def __init__(self, conv_shape, network_depth, no_convolutions, conv_filter_no_init,
-                 conv_kernel_size, latent_representation_dim, output_channels=2, l1=0.0, l2=0.0,
+                 conv_kernel_size, latent_representation_dim, output_channels=5, l1=0.0, l2=0.0,
                  dropout_value=0.0, use_batch_normalization=False, activation='relu'):
         super(Decoder3D, self).__init__()
-        self.conv_shape = conv_shape  # Shape of the feature map at the start of the decoder
+        self.conv_shape = conv_shape
         self.network_depth = network_depth
         self.no_convolutions = no_convolutions
         self.conv_filter_no_init = conv_filter_no_init
@@ -108,9 +121,8 @@ class Decoder3D(nn.Module):
         self.dropout_value = dropout_value
         self.use_batch_normalization = use_batch_normalization
         self.activation = activation
-        self.output_channels = output_channels  # Final output channels (e.g., 5 channels for MRI modalities)
+        self.output_channels = output_channels
         
-        # Activation function
         if activation == 'leakyrelu':
             self.activation_fn = nn.LeakyReLU(inplace=True)
         else:
@@ -124,47 +136,38 @@ class Decoder3D(nn.Module):
         
         # Decoder layers (upsample and conv layers)
         self.decoder_layers = nn.ModuleList()
+        in_channels = conv_shape[0]
         
-        # Reverse the depth, so we progressively upsample back to the original image size
-        in_channels = conv_shape[0]  # Start with the number of channels from the conv_shape
         for i in reversed(range(network_depth)):
             # Upsampling layer
             upsample_factors = downsample_fn(network_depth)[i]
             self.decoder_layers.append(nn.Upsample(scale_factor=upsample_factors, mode='trilinear', align_corners=False))
             
-            # Convolution layers
-            out_channels = self.conv_filter_no_init * (2 ** i)  # Reduce the number of channels as we move up the network
-            for j in range(no_convolutions):
-                print(f"Layer {i}-{j}: in_channels = {in_channels}, out_channels = {out_channels}")
+            # First convolution layer after concatenation needs to handle in_channels + out_channels channels
+            out_channels = self.conv_filter_no_init * (2 ** i)
+            self.decoder_layers.append(nn.Conv3d(in_channels+out_channels, out_channels, kernel_size=conv_kernel_size, padding=(conv_kernel_size-1) // 2))
+            in_channels = out_channels
+            for j in range(1, no_convolutions):
                 self.decoder_layers.append(nn.Conv3d(in_channels, out_channels, kernel_size=conv_kernel_size, padding=(conv_kernel_size-1) // 2))
-                if use_batch_normalization:
-                    self.decoder_layers.append(nn.BatchNorm3d(out_channels))
-                if activation == 'leakyrelu':
-                    self.decoder_layers.append(nn.LeakyReLU(inplace=True))
-                else:
-                    self.decoder_layers.append(nn.ReLU(inplace=True))
-                in_channels = out_channels  # Update in_channels for the next layer
-            if dropout_value > 0.0:
-                self.decoder_layers.append(nn.Dropout3d(p=dropout_value))
+                
+            
         
-        # Final convolution to produce the reconstructed image with the correct number of output channels
-        print(f"Final Layer: in_channels = {in_channels}, out_channels = {self.output_channels}")
         self.final_conv = nn.Conv3d(in_channels, self.output_channels, conv_kernel_size, padding=(conv_kernel_size-1) // 2)
-        self.final_activation = nn.ReLU()  # You could change this to `nn.Sigmoid()` or `nn.Tanh()` depending on the data range
+        self.final_activation = nn.ReLU()
 
-    def forward(self, x):
-        # Expand the latent vector via the fully connected layer
+    def forward(self, x, skip):
         x = self.fc(x)
         x = self.activation_fn(x)
+        x = self.reshape(x)
         
-        # Reshape to the shape required for the convolutional layers
-        x = self.reshape(x)  # Reshape to 3D tensor (batch_size, channels, depth, height, width)
-        
-        # Apply the decoder layers
-        for layer in self.decoder_layers:
+        for i, layer in enumerate(self.decoder_layers):
+            if isinstance(layer, nn.Upsample):
+                skip_connection = skip.pop()
+                x = F.interpolate(x, size=skip_connection.shape[2:], mode='trilinear', align_corners=False)
+                x = torch.cat((x, skip_connection), dim=1)
             x = layer(x)
-        
-        # Final convolution to produce the output volume
+            # if isinstance(layer, nn.Conv3d):
+            #     print(f"Shape after Conv3D {i}: {x.shape}, expected input: {layer.weight.shape[1]}, expected output: {layer.weight.shape[0]}")
         x = self.final_conv(x)
         x = self.final_activation(x)
         return x
