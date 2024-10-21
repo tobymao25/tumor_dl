a37c226dc26a314e19ceec3925eed517d89f4065
Linda  Tang <ltang35@mrphpch1.cm.cluster>
2024-10-21 11:35:20 -0400

    added validation for resnet

--- image_branch_train_resnet.py
+++ image_branch_train_resnet.py
@@ -1,10 +1,12 @@
 import os
 import torch
+import pandas as pd
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
 from datetime import datetime
+from sklearn.model_selection import train_test_split
 from image_branch_utils import GBMdataset
 from image_branch_resnet import ResNet3D, ResidualBlock, get_resnet_layers
 
@@ -15,7 +17,7 @@ def plot_loss_curves(loss_plot_out_dir, train_epoch_losses):
 
     train_losses = train_epoch_losses
     train_losses = train_losses[1:] # do not plot the loss of the first epoch
-    plt.plot([x+1 for x in range(len(train_losses))], train_losses, label="train", color="blue", lw=3)
+    plt.plot([x+1 for x in range(len(train_losses))], train_losses, label="train", color="blue", lw=2)
 
     plt.xlabel('Epochs')
     plt.ylabel('Loss')
@@ -24,7 +26,7 @@ def plot_loss_curves(loss_plot_out_dir, train_epoch_losses):
     # Get the current timestamp
     timestamp = datetime.now().strftime('%Y%m%d')
     # Save images
-    plt.savefig(os.path.join(loss_plot_out_dir, f"Loss {timestamp}.png"))
+    plt.savefig(os.path.join(loss_plot_out_dir, f"Loss_{timestamp}.png"))
 
 
 def train_resnet(config): 
@@ -33,10 +35,22 @@ def train_resnet(config):
     image_dir = "/home/ltang35/tumor_dl/TrainingDataset/images"
     csv_path = "/home/ltang35/tumor_dl/TrainingDataset/survival_data_fin.csv"
     loss_plot_out_dir = "/home/ltang35/tumor_dl/TrainingDataset/out"
+    train_csv_path = "/home/ltang35/tumor_dl/TrainingDataset/survival_data_fin_train.csv"
+    valid_csv_path = "/home/ltang35/tumor_dl/TrainingDataset/survival_data_fin_test.csv"
+
+    # train valid split
+    all_patient_data_df = pd.read_csv(csv_path)
+    random_seed = 11
+    print("the random seed is 11")
+    train_df, valid_df = train_test_split(all_patient_data_df, test_size=0.1, random_state=random_seed)
+    train_df.to_csv(train_csv_path, index=False)
+    valid_df.to_csv(valid_csv_path, index=False)
 
     # setup training and validation datasets
-    dataset = GBMdataset(image_dir=image_dir, csv_path=csv_path)#, transform=transform)
-    dataloader = DataLoader(dataset, batch_size=config['batch_size'], shuffle=True, num_workers=4)
+    train_dataset = GBMdataset(image_dir=image_dir, csv_path=train_csv_path)#, transform=transform)
+    train_dataloader = DataLoader(train_dataset, batch_size=4, shuffle=True, num_workers=4)
+    valid_dataset = GBMdataset(image_dir=image_dir, csv_path=valid_csv_path)#, transform=transform)
+    valid_dataloader = DataLoader(valid_dataset, batch_size=4, shuffle=True, num_workers=4)
    
     # setup device
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -63,8 +77,8 @@ def train_resnet(config):
         print(f"Current epoch: {epoch+1}")
         running_losses = 0.0
         model.train()
-        for i, (inputs, survival_times) in enumerate(dataloader):
-            print("batch", i, "out of", len(dataloader))
+        for i, (inputs, survival_times) in enumerate(train_dataloader):
+            print("batch", i, "out of", len(train_dataloader))
             # print("Model parameters:")
             # for param in model.parameters():
             #     print(param)
@@ -74,6 +88,7 @@ def train_resnet(config):
             survival_times = survival_times.to(device)
             
             outputs = model(inputs)
+            outputs = outputs.squeeze() # Ensure mu and x has same dimension
             print("here are the predictions", outputs)
             print("here are the labels", survival_times)
             loss = criterion(outputs, survival_times)
@@ -86,14 +101,70 @@ def train_resnet(config):
             running_losses += loss.item() * inputs.size(0) # in case the last batch has fewer samples
 
         # Report and print epoch losses
-        avg_loss = running_losses / len(dataloader.dataset)
+        avg_loss = running_losses / len(train_dataloader.dataset)
         # if key == "loss":
         #     train.report({key: avg_loss})
         print(f"Epoch {epoch+1}/{epochs}, loss: {avg_loss:.4f}")
         epoch_losses.append(avg_loss)
 
+        # Save the model checkpoint every 30 epochs
+        if epoch % 30 == 0:
+            torch.save(model.state_dict(), f'/home/ltang35/tumor_dl/TrainingDataset/out/resnet_epoch_{epoch}.pt')
+
+        # plot loss curve for training
         plot_loss_curves(loss_plot_out_dir, epoch_losses) # plot loss curves after each epoch
 
+        # Validate every 20 epoches
+        epoch_validated = []
+        epoch_valid_losses = []
+        if epoch % 3 == 0:
+            print(f"--Validation for epoch {epoch+1}--")
+            model.eval()
+            with torch.no_grad():
+                running_losses = 0.0
+                model.train()
+                for i, (inputs, survival_times) in enumerate(valid_dataloader):
+                    print("batch", i, "out of", len(valid_dataloader))
+                    # print("Model parameters:")
+                    # for param in model.parameters():
+                    #     print(param)
+                    # process inputs data
+                    inputs = inputs.to(device)
+                    inputs = inputs.squeeze(2) # remove 3rd dimension [n, 5, 1, 128, 128, 128]
+                    survival_times = survival_times.to(device)
+                    
+                    outputs = model(inputs)
+                    outputs = outputs.squeeze() # Ensure mu and x has same dimension
+                    print("here are the predictions", outputs)
+                    print("here are the labels", survival_times)
+                    loss = criterion(outputs, survival_times)
+                    print("This is the calculated loss", loss)
+
+                    running_losses += loss.item() * inputs.size(0) # in case the last batch has fewer samples
+
+                # Report and print epoch losses
+                avg_loss = running_losses / len(train_dataloader.dataset)
+                print(f"Epoch {epoch+1}/{epochs}, loss: {avg_loss:.4f}")
+                epoch_validated.append(epoch+1)
+                epoch_valid_losses.append(avg_loss)
+
+                print("epoch_validated", epoch_validated)
+                print("epoch_valid_losses", epoch_valid_losses)
+
+                #### plotting function
+                plt.figure(figsize=(5, 3))
+                plt.plot(epoch_validated, epoch_valid_losses, label="valid", color="red", lw=2)
+                plt.xlabel('Epochs')
+                plt.ylabel('Loss')
+                plt.legend()
+
+                # Get the current timestamp
+                timestamp = datetime.now().strftime('%Y%m%d')
+                # Save images
+                plt.savefig(os.path.join(loss_plot_out_dir, f"Loss_Valid_{timestamp}.png"))
+
+            print(f"--Validation finished for epoch {epoch+1}--")
+        
         # Clear GPU cache
         torch.cuda.empty_cache()
 
